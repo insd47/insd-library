694d58ac80156a30f21e5695fadadf737970f6f0
Inseong <67549204+insd47@users.noreply.github.com>
2023-08-28 12:00:03 +0900

    fix: ignored export

--- src/components/generic/index.ts
+++ src/components/generic/index.ts
@@ -1,3 +1,4 @@
+export { default as Box } from "./Box";
 export { default as Icon } from "./Icon";
 export { default as Loading } from "./Loading";
 export { default as Disabler } from "./Disabler";
