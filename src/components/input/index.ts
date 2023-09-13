11ed410e9e41713131c0307422f99a74993f8c33
insd47 <me@insd.dev>
2023-09-14 01:02:22 +0900

    fix: missing export of boolean

--- src/components/input/index.ts
+++ src/components/input/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from "./Button";
 export { default as Textfield } from "./Textfield";
+export { default as Boolean } from "./Boolean";
 
 export type { ButtonType, ButtonSize } from "./Button/types";
