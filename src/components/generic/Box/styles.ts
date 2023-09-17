8dbfa482255d22ce5bb139d87552d2c0fc6423f7
insd47 <me@insd.dev>
2023-09-17 16:59:45 +0900

    feat: advanced box styles

--- src/components/generic/Box/styles.ts
+++ src/components/generic/Box/styles.ts
@@ -5,11 +5,59 @@ const StyledBox = styled.div<StyledBoxProps>`
   box-sizing: border-box;
   border: 1px solid transparent;
 
-  ${({ background, border, radius }) => ({
-    backgroundColor: background,
-    borderColor: border,
-    borderRadius: radius,
-  })}
+  ${({ theme, background }) => {
+    const backgroundStyles = {
+      bg: theme.colors.box.background + "",
+      fg1: theme.colors.box.foreground[1] + "",
+      fg2: theme.colors.box.foreground[2] + "",
+      fg3: theme.colors.box.foreground[3] + "",
+    };
+
+    return background && `background-color: ${backgroundStyles[background]};`;
+  }}
+
+  ${({ theme, border }) => {
+    const borderStyles = {
+      1: "1px solid " + theme.colors.box.border[1],
+      2: "1px solid " + theme.colors.box.border[2],
+      3: "1px solid " + theme.colors.box.border[3],
+    };
+
+    if (typeof border === "number") return `border: ${borderStyles[border]};`;
+
+    return (
+      border && {
+        borderTop: border.top && borderStyles[border.top],
+        borderRight: border.right && borderStyles[border.right],
+        borderBottom: border.bottom && borderStyles[border.bottom],
+        borderLeft: border.left && borderStyles[border.left],
+      }
+    );
+  }}
+
+  ${({ theme, radius }) => {
+    const radiusStyles = {
+      1: theme.variables.radius[1],
+      2: theme.variables.radius[2],
+      3: theme.variables.radius[3],
+      4: theme.variables.radius[4],
+      5: theme.variables.radius[5],
+    };
+
+    if (typeof radius === "number")
+      return `border-radius: ${radiusStyles[radius]};`;
+
+    return (
+      radius && {
+        borderTopLeftRadius: radius.topLeft && radiusStyles[radius.topLeft],
+        borderTopRightRadius: radius.topRight && radiusStyles[radius.topRight],
+        borderBottomLeftRadius:
+          radius.bottomLeft && radiusStyles[radius.bottomLeft],
+        borderBottomRightRadius:
+          radius.bottomRight && radiusStyles[radius.bottomRight],
+      }
+    );
+  }}}
 
   ${({ CSSValues }) => ({ ...CSSValues })}
 `;
--- src/components/generic/Box/types.ts
+++ src/components/generic/Box/types.ts
@@ -1,22 +1,34 @@
 import { CSSProperties, HTMLAttributes } from "react";
 
-interface CommonProps extends HTMLAttributes<HTMLDivElement> {}
+type borderType = 1 | 2 | 3;
+type radiusType = 1 | 2 | 3 | 4 | 5;
+
+interface CommonProps extends HTMLAttributes<HTMLDivElement> {
+  background?: "bg" | "fg1" | "fg2" | "fg3";
+  border?:
+    | borderType
+    | {
+        top?: borderType;
+        right?: borderType;
+        bottom?: borderType;
+        left?: borderType;
+      };
+  radius?:
+    | radiusType
+    | {
+        topLeft?: radiusType;
+        topRight?: radiusType;
+        bottomLeft?: radiusType;
+        bottomRight?: radiusType;
+      };
+}
 
 export interface BoxProps extends CommonProps {
   margin?: CSSProperties["margin"];
   padding?: CSSProperties["padding"];
-  background?: "bg" | "fg1" | "fg2" | "fg3";
-  border?: 1 | 2 | 3;
-  radius?: 1 | 2 | 3 | 4 | 5;
-  customBackground?: CSSProperties["backgroundColor"];
-  customBorder?: CSSProperties["borderColor"];
-  customRadius?: CSSProperties["borderRadius"];
   overflow?: CSSProperties["overflow"];
 }
 
 export interface StyledBoxProps extends CommonProps {
   CSSValues?: CSSProperties;
-  background: CSSProperties["backgroundColor"];
-  border: CSSProperties["borderColor"];
-  radius: CSSProperties["borderRadius"];
 }
