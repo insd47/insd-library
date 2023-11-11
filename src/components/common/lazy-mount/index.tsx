40f7b2efdb2dacc500c79c5f1d260b0615ce9fec
insd47 <me@insd.dev>
2023-11-12 02:18:34 +0900

    fix: missing react import

--- src/components/common/lazy-mount/index.tsx
+++ src/components/common/lazy-mount/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useLayoutEffect } from "react";
+import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
 
 interface LazyMountProps {
   enabled: boolean;
